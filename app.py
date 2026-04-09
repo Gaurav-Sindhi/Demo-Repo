f1d5559b6754420dee9ce399eaecdb1341dab19c
Gaurav-Sindhi <gaurav.sindhi2703@gmail.com>
2026-04-09 14:41:27 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def main():
+    print("Simple Python CI Demo App")
+    print("Addition: ", add(5, 3))
+    print("Subtraction: ", subtract(5, 3))
+
+if __name__ == "__main__":
+    main()
